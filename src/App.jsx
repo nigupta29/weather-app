318826f50ec45ef081c21949d49b448361e1fde9
Nikhil Gupta <nikhil.gupta5667@gmai.com>
2023-10-28 23:00:28 +0530

    reset on the a[[

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import bgImage from './assets/images/landscape.jpg'
 import SearchBox from './components/SearchBox'
 import WeatherCard from './components/WeatherCard'
+
+const mainScreenStyles = {
+  backgroundImage: `url(${bgImage})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center'
+}
 
 function App() {
   const [weatherData, setWeatherData] = useState({})
 
   return (
-    <div className="">
+    <div className="" style={mainScreenStyles}>
       <SearchBox setWeatherData={setWeatherData} />
       <WeatherCard weatherData={weatherData} />
     </div>
